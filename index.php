a383d958df4d63373105683fba4a98cdb5daf47b
Adriano <Adriano@192.168.25.167>
2016-04-04 23:12:57 -0300

    bootstrap sass em construção [card-calendar]

--- index.php
+++ index.php
@@ -1,13 +1,70 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>SASS</title>
         <link rel="stylesheet" href="css/default.css"/>
     </head>
-    <body>  
-        <div class="row">
-            <div class="message success">
-                mensagem de erro
+    <body>
+        
+        <div class="column-2">
+            <div class="card-calendar card-success">
+                <div class="header">
+                    <h1>1</h1>
+                </div>
+
+                <div class="container">
+                    <p>January 1, 2016</p>
+                </div>
             </div>
-        </div>        
+        </div>
+        
+        <div class="column-2">
+            <div class="card-calendar card-primary">
+                <div class="header">
+                    <h1>1</h1>
+                </div>
+
+                <div class="container">
+                    <p>January 1, 2016</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="column-2">
+            <div class="card-calendar card-alert">
+                <div class="header">
+                    <h1>1</h1>
+                </div>
+
+                <div class="container">
+                    <p>January 1, 2016</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="column-2">
+            <div class="card-calendar card-danger">
+                <div class="header">
+                    <h1>1</h1>
+                </div>
+
+                <div class="container">
+                    <p>January 1, 2016</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="column-2">
+            <div class="card-calendar card-info">
+                <div class="header">
+                    <h1>1</h1>
+                </div>
+
+                <div class="container">
+                    <p>January 1, 2016</p>
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
