da6ea8f694ed82603f5be050a2ed36c8763dde93
Adriano silva dos Reis <adriano_silvareis@hotmail.com>
2016-04-06 17:56:24 -0300

    Form + buttons + inputs + gerenciamento de cores

--- index.php
+++ index.php
@@ -3,31 +3,14 @@
     <head>
         <title>SASS</title>
         <link rel="stylesheet" href="css/default.css"/>
-        <link rel="stylesheet" href="css/compile.css"/>
     </head>
     <body>
-        <header class="header bg-success"> 
-            <h1 class="logo">Titulo do site</h1>
-        </header>
-
-        <main class="content column-8 bg-info">
-            <div class="column-4">
-                <div class="card-calendar">
-                    <header class="card-header">1</header>
-                    <p class="card-container">1 de janeiro</p>
-                </div>
-            </div>
-        </main>
-        <aside class="column-4 bg-danger">
-            <div class="column-12">
-                <div class="card-calendar">
-                    <header class="card-header">1</header>
-                    <p class="card-container">1 de janeiro</p>
-                </div>
-            </div>
-        </aside>
-        <footer class="footer-md bg-warning">
-            footer
-        </footer>
+        <form>
+             <input class="btn btn-success" type='submit' placeholder="nome" />
+             <input class="btn btn-primary" type='submit' placeholder="nome" />
+             <input class="btn btn-info" type='submit' placeholder="nome" />
+             <input class="btn btn-danger" type='submit' placeholder="nome" />
+             <input class="btn btn-warning" type='submit' placeholder="nome" />
+        </form>
     </body>
 </html>
