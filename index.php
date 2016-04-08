d1c0df60300b9f9bcd2d0b87b6964fb350c7879f
Adriano silva dos Reis <adriano_silvareis@hotmail.com>
2016-04-08 17:59:18 -0300

    menus e tables e alteração de outras classes.

--- index.php
+++ index.php
@@ -5,12 +5,65 @@
         <link rel="stylesheet" href="css/default.css"/>
     </head>
     <body>
-        <form>
-             <input class="btn btn-success" type='submit' placeholder="nome" />
-             <input class="btn btn-primary" type='submit' placeholder="nome" />
-             <input class="btn btn-info" type='submit' placeholder="nome" />
-             <input class="btn btn-danger" type='submit' placeholder="nome" />
-             <input class="btn btn-warning" type='submit' placeholder="nome" />
-        </form>
+        <header class="header bg-success">
+            <h1>Nome do Site</h1>
+            <ul>
+                <li class="icons icons-accept">teste</li>
+                <li class="icons icons-act_ative">teste</li>
+                <li class="icons icons-act_edit">teste</li>
+                <li class="icons icons-act_view">teste</li>
+                <li class="icons icons-alert">teste</li>
+            </ul>
+
+            
+        </header>
+        <div style="height: 100px;"></div>
+        <section class="section">
+
+            <main class="column-8">
+                <h1>Conteudo do meu site</h1>
+                <table class="table table-warning">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Telefone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Adriano</td>
+                            <td>3022-0117</td>
+                        </tr>
+                        <tr>
+                            <td>Adriano</td>
+                            <td>3022-0117</td>
+                        </tr>
+                        <tr>
+                            <td>Adriano</td>
+                            <td>3022-0117</td>
+                        </tr>
+                        <tr>
+                            <td>Adriano</td>
+                            <td>3022-0117</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </main>
+
+            <aside class="column-4">
+                <h1>Meu aside</h1>
+                <ul class="menu menu-danger">
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </aside>
+
+        </section>
+
+        <footer class="footer-lg bg-success">
+            rodapé
+        </footer>
     </body>
 </html>
